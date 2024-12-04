9ab516ee9948b1619745ef6a3288cc0f5496fecb
Klim939 <klimborisov9552@gmail.com>
2024-12-04 13:07:02 +0300

    2 commit: added get_location_key

--- weather_funcs.py
+++ weather_funcs.py
@@ -5,3 +5,15 @@ def load_config():
         data = f.read().split('\n')
         global API_KEY
         API_KEY = data[0].split()[1]
+
+load_config()
+
+def get_location_key(city_name):
+    url = f'http://dataservice.accuweather.com/locations/v1/cities/search?apikey={API_KEY}&q={city_name}&details=true'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()[0]
+        return data.get('Key')
+    else:
+        return None
