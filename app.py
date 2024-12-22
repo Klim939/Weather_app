195be4ed097444f5445ff6d2bca363b488ae377f
Klim939 <klimborisov9552@gmail.com>
2024-12-22 21:17:35 +0300

    v1.1.0 Added days choose

--- app.py
+++ app.py
@@ -19,39 +19,22 @@ def error(error_message):
 def results():
     start_city = request.form.get('start_city')
     end_city = request.form.get('end_city')
+    stop_cities = request.form.getlist('stop_city')
+    days = int(request.form.get('days', 5))
+    cities = [start_city] + stop_cities + [end_city]
+    route_forecasts = {}
+    route_coordinates = []
 
-    start_weather = weather_model(start_city)
-    end_weather = weather_model(end_city)
-    if type(start_weather) == str:
-        return error(error_message=start_weather)
-    elif type(end_weather) == str:
-        return error(error_message=end_weather)
-    elif not(start_weather and end_weather):
-        return error(error_message='Ошибка: нет данных по погоде')
-    else:
-        return render_template('result.html',
-                               start_city=start_city,
-                               start_condition=start_weather['message'],
-                               start_r_temperature=start_weather['temperature']['real'],
-                               start_f_temperature=start_weather['temperature']['feel'],
-                               start_wind_speed=start_weather['wind']['speed'],
-                               start_direction=start_weather['wind']['direction'],
-                               start_humidity=start_weather['humidity'],
-                               start_precib_prob=start_weather['precip_prob'],
-                               start_liquid=start_weather['total_liq'],
-                               start_cloud_cover=start_weather['cloud_cover'],
-
-                               end_city=end_city,
-                               end_condition=end_weather['message'],
-                               end_r_temperature=end_weather['temperature']['real'],
-                               end_f_temperature=end_weather['temperature']['feel'],
-                               end_wind_speed=end_weather['wind']['speed'],
-                               end_direction=end_weather['wind']['direction'],
-                               end_humidity=end_weather['humidity'],
-                               end_precib_prob=end_weather['precip_prob'],
-                               end_liquid=end_weather['total_liq'],
-                               end_cloud_cover=end_weather['cloud_cover']
-                               )
+    for city in cities:
+        route_forecasts[city] = get_weather_data(city, days)
+        print(route_forecasts[city])
+        if type(route_forecasts[city]) == str:
+            return error(error_message=route_forecasts[city])
+        elif not route_forecasts[city]:
+            return error(error_message='Ошибка: нет данных по погоде')
+    forecast_data = [{"city": city, "forecasts": forecast} for city, forecast in route_forecasts.items()]
+    print(1)
+    return render_template('result.html', forecast_data=forecast_data, days=days)
 
 if __name__ == '__main__':
     run_simple('localhost', 5000, app)
--- weather_funcs.py
+++ weather_funcs.py
@@ -17,28 +17,35 @@ def get_location_key(city_name: str):
     else:
         return None
 
-def get_weather_data(city_name: str):
+def get_weather_data(city_name: str, days=1):
     try:
         location_key = get_location_key(city_name)
         if location_key == None:
             return 'Ошибка: не удалось получить ключ города'
         else:
-            weather_url = f'http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/{location_key}?apikey={API_KEY}&details=true&metric=true'
-            weather_response = requests.get(weather_url)
-            if weather_response.status_code == 200 and weather_response.json():
-                data = weather_response.json()[0]
-                return {
-                    'temperature': {'real': data['Temperature']['Value'],
-                                    'feel': data['RealFeelTemperature']['Value']},
-                    'wind': {'speed': data['Wind']['Speed']['Value'],
-                             'direction': data['Wind']['Direction']['Localized'].replace('N','С').replace('S','Ю').replace('W','З').replace('E','В')},
-                    'humidity': data['RelativeHumidity'],
-                    'precip_prob': data['PrecipitationProbability'],
-                    'total_liq': data['TotalLiquid']['Value'],
-                    'cloud_cover': data['CloudCover']
-                }
+            forecast_url = f"http://dataservice.accuweather.com/forecasts/v1/daily/{f"{days}day"}/{location_key}?apikey={API_KEY}&metric=true&details=true"
+            forecast_response = requests.get(forecast_url)
+            print(forecast_response.json())
+            daily_forecasts = []
+            if forecast_response.status_code == 200 and forecast_response.json():
+                for day in forecast_response.json()['DailyForecasts']:
+                    data = day
+                    daily_forecasts.append({
+                        "date": data["Date"],
+                        'temperature': {'real': round((data['Temperature']['Minimum']['Value'] + data['Temperature']['Maximum']['Value']) / 2),
+                                        'feel': round((data['RealFeelTemperature']['Minimum']['Value'] + data['RealFeelTemperature']['Maximum']['Value']) / 2)},
+                        'wind': {'speed': data['Day']['Wind']['Speed']['Value'],
+                                 'direction': data['Day']['Wind']['Direction']['Localized'].replace('N', 'С').replace('S',
+                                                                                                               'Ю').replace(
+                                     'W', 'З').replace('E', 'В')},
+                        'humidity': data['Day']['RelativeHumidity']['Average'],
+                        'precip_prob': data['Day']['PrecipitationProbability'],
+                        'total_liq': data['Day']['TotalLiquid']['Value'],
+                        'cloud_cover': data['Day']['CloudCover']
+                    })
             else:
                 return 'Ошибка: не удалось получить данные о погоде'
+        return daily_forecasts
     except Exception as e:
         return f'Error, key: {e}'
 
