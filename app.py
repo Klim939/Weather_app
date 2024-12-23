5d075be77d5ba4fd3c32dcc745b7b28e2fe3d653
Klim939 <klimborisov9552@gmail.com>
2024-12-23 14:26:18 +0300

    v 2.0.1, Added folium map

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
 from flask import Flask, render_template, request
 from werkzeug.serving import run_simple
-from weather_funcs import load_config, get_location_key, get_weather_data, weather_model
+from weather_funcs import load_config, get_location_key, get_weather_data
+import folium
 
 API_KEY = ''
 app = Flask(__name__, template_folder='templates')
@@ -26,15 +27,74 @@ def results():
     route_coordinates = []
 
     for city in cities:
-        route_forecasts[city] = get_weather_data(city, days)
-        print(route_forecasts[city])
+        forecast, latitude, longitude = get_weather_data(city, days)
+        route_forecasts[city] = forecast
         if type(route_forecasts[city]) == str:
             return error(error_message=route_forecasts[city])
         elif not route_forecasts[city]:
             return error(error_message='Ошибка: нет данных по погоде')
+        route_coordinates.append((latitude, longitude))
+        if route_coordinates:
+            route_map = folium.Map(location=route_coordinates[0], zoom_start=6)
+            route_map.fit_bounds(route_coordinates)
+        else:
+            # Если нет координат, используем координаты Москвы
+            route_map = folium.Map(location=(55.751244, 37.618423), zoom_start=6)
+
+            # Добавление линии маршрута на карту с более заметным стилем
+        if route_coordinates:
+            folium.PolyLine(
+                locations=route_coordinates,
+                color='black',
+                weight=3,
+                opacity=0.8
+            ).add_to(route_map)
+
+            # Добавление маркеров на карту для каждого города с кратким прогнозом
+        for idx, (city, forecast) in enumerate(route_forecasts.items()):
+            lat, lon = forecast[0].get('latitude'), forecast[0].get('longitude')
+
+            if lat is not None and lon is not None:
+                popup_text = f"<b>{city}</b><br>Температура: {forecast[0]['temperature']['feel']}°C<br>Осадки: {forecast[0]['precip_prob']}%<br>Влажность: {forecast[0]['humidity']}%<br>Скорость ветра: {forecast[0]['wind']['speed']} м/c"
+                # Используем CircleMarker для выделения городов
+                if idx == 0:
+                    # Начальный город
+                    folium.CircleMarker(
+                        location=[lat, lon],
+                        radius=10,
+                        color='red',
+                        fill=True,
+                        fill_color='red',
+                        popup=popup_text,
+                        tooltip=city+f" Температура: {forecast[0]['temperature']['feel']}°C"
+                    ).add_to(route_map)
+                elif idx == len(route_forecasts) - 1:
+                    # Конечный город
+                    folium.CircleMarker(
+                        location=[lat, lon],
+                        radius=8,
+                        color='red',
+                        fill=True,
+                        fill_color='red',
+                        popup=popup_text,
+                        tooltip=city+f" Температура: {forecast[0]['temperature']['feel']}°C"
+                    ).add_to(route_map)
+                else:
+                    # Промежуточные города
+                    folium.CircleMarker(
+                        location=[lat, lon],
+                        radius=5,
+                        color='black',
+                        fill=True,
+                        fill_color='black',
+                        popup=popup_text,
+                        tooltip=city+f" Температура: {forecast[0]['temperature']['feel']}°C"
+                    ).add_to(route_map)
+
+        # Получаем HTML-представление карты
+    map_html = route_map._repr_html_()
     forecast_data = [{"city": city, "forecasts": forecast} for city, forecast in route_forecasts.items()]
-    print(1)
-    return render_template('result.html', forecast_data=forecast_data, days=days)
+    return render_template('result.html', forecast_data=forecast_data, days=days, folium_map=map_html)
 
 if __name__ == '__main__':
     run_simple('localhost', 5000, app)
--- weather_funcs.py
+++ weather_funcs.py
@@ -13,19 +13,18 @@ def get_location_key(city_name: str):
 
     if response.status_code == 200 and response.json():
         data = response.json()[0]
-        return data.get('Key')
+        return data.get('Key'), data['GeoPosition']['Latitude'], data['GeoPosition']['Longitude'],
     else:
         return None
 
 def get_weather_data(city_name: str, days=1):
     try:
-        location_key = get_location_key(city_name)
+        location_key, latitude, longitude = get_location_key(city_name)
         if location_key == None:
             return 'Ошибка: не удалось получить ключ города'
         else:
             forecast_url = f"http://dataservice.accuweather.com/forecasts/v1/daily/{f"{days}day"}/{location_key}?apikey={API_KEY}&metric=true&details=true"
             forecast_response = requests.get(forecast_url)
-            print(forecast_response.json())
             daily_forecasts = []
             if forecast_response.status_code == 200 and forecast_response.json():
                 for day in forecast_response.json()['DailyForecasts']:
@@ -41,100 +40,13 @@ def get_weather_data(city_name: str, days=1):
                         'humidity': data['Day']['RelativeHumidity']['Average'],
                         'precip_prob': data['Day']['PrecipitationProbability'],
                         'total_liq': data['Day']['TotalLiquid']['Value'],
-                        'cloud_cover': data['Day']['CloudCover']
+                        'cloud_cover': data['Day']['CloudCover'],
+                        'latitude': latitude,
+                        'longitude': longitude
                     })
             else:
                 return 'Ошибка: не удалось получить данные о погоде'
-        return daily_forecasts
+        return daily_forecasts, latitude, longitude
     except Exception as e:
-        return f'Error, key: {e}'
+        return f'Error, key: {e}', None, None
 
-def weather_model(city_name: str):
-    data = get_weather_data(city_name)
-    try:
-        bad_weather_coef = 0
-        temp = data['temperature']['real']
-        wind_speed = data['wind']['speed']
-        humidity = data['humidity']
-        precip_prob = data['precip_prob']
-        total_liq = data['total_liq']
-        cloud_cover = data['cloud_cover']
-
-        # температура
-        if temp < -20:
-            bad_weather_coef += 2
-        elif temp < -10:
-            bad_weather_coef += 1.5
-        elif temp < 0:
-            bad_weather_coef += 1
-        elif temp < 10:
-            bad_weather_coef += 0.5
-        elif temp > 30:
-            bad_weather_coef += 0.5
-        elif temp > 35:
-            bad_weather_coef += 1
-        elif temp > 40:
-            bad_weather_coef += 1.5
-        elif temp > 45:
-            bad_weather_coef += 2
-
-        # ветер
-        if wind_speed >= 18.3:
-            bad_weather_coef += 10
-        elif wind_speed >= 15.3:
-            bad_weather_coef += 2
-        elif wind_speed >= 12.5:
-            bad_weather_coef += 1.5
-        elif wind_speed >= 9.9:
-            bad_weather_coef += 1
-        elif wind_speed >= 7.5:
-            bad_weather_coef += 0.5
-
-        #влажность
-        if humidity > 80:
-            bad_weather_coef += 0.5
-
-        #осадки
-        if precip_prob >= 90:
-            bad_weather_coef += 1
-        elif precip_prob >= 75:
-            bad_weather_coef += 0.5
-        elif precip_prob >= 50:
-            bad_weather_coef += 0.2
-
-        #уровень осадков
-        if total_liq >= 50:
-            bad_weather_coef += 1.5
-        elif total_liq >= 32:
-            bad_weather_coef += 1
-        elif total_liq >= 15:
-            bad_weather_coef += 0.5
-
-        #облачность
-        if cloud_cover >= 90:
-            bad_weather_coef += 0.3
-        if cloud_cover >= 50:
-            bad_weather_coef += 0.2
-        if cloud_cover >= 25:
-            bad_weather_coef += 0.1
-
-        #итог
-        if bad_weather_coef >= 6:
-            data['message'] = 'Критические условия, не рекомендуем путешествовать'
-        elif bad_weather_coef >= 5:
-            data['message'] = 'Крайне неблагоприятные условия, не рекомендуем путешествовать'
-        elif bad_weather_coef >= 4:
-            data['message'] = 'Неблагоприятные условия для путешествия'
-        elif bad_weather_coef >= 3:
-            data['message'] = 'Не очень благоприятные условия для путешествия'
-        elif bad_weather_coef >= 2:
-            data['message'] = 'Удовлетворительные уловия для путешествия'
-        else:
-            data['message'] = 'Благоприятные условия для путешествия'
-
-    except Exception as e:
-        return data
-
-    return data
-
-print(weather_model('Москва'))
